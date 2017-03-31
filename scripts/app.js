f7941de51d493639e57a591a91237742f8993d94
Lalith narayan Kashyap <lalith@moonraft.com>
2017-03-31 11:51:18 +0530

    first commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,143 +1,4 @@
-// var Pagination = {
-
-//     code: '',
-
-//     // --------------------
-//     // Utility
-//     // --------------------
-
-//     // converting initialize data
-//     Extend: function(data) {
-//         debugger;
-//         data = data || {};
-//         Pagination.size = data.size || 30;
-//         Pagination.page = data.page || 1;
-//         Pagination.step = data.step || 3;
-//     },
-
-//     // add pages by number (from [s] to [f])
-//     Add: function(s, f) {
-//         for (var i = s; i < f; i++) {
-//             Pagination.code += '<a>' + i + '</a>';
-//         }
-//     },
-
-//     // add last page with separator
-//     Last: function() {
-//         Pagination.code += '<i>...</i><a>' + Pagination.size + '</a>';
-//     },
-
-//     // add first page with separator
-//     First: function() {
-//         Pagination.code += '<a>1</a><i>...</i>';
-//     },
-
-
-
-//     // --------------------
-//     // Handlers
-//     // --------------------
-
-//     // change page
-//     Click: function() {
-//         Pagination.page = +this.innerHTML;
-//         Pagination.Start();
-//     },
-
-//     // previous page
-//     Prev: function() {
-//         Pagination.page--;
-//         if (Pagination.page < 1) {
-//             Pagination.page = 1;
-//         }
-//         Pagination.Start();
-//     },
-
-//     // next page
-//     Next: function() {
-//         Pagination.page++;
-//         if (Pagination.page > Pagination.size) {
-//             Pagination.page = Pagination.size;
-//         }
-//         Pagination.Start();
-//     },
-
-
-
-//     // --------------------
-//     // Script
-//     // --------------------
-
-//     // binding pages
-//     Bind: function() {
-//         var a = Pagination.e.getElementsByTagName('a');
-//         for (var i = 0; i < a.length; i++) {
-//             if (+a[i].innerHTML === Pagination.page) a[i].className = 'current';
-//             a[i].addEventListener('click', Pagination.Click, false);
-//         }
-//     },
-
-//     // write pagination
-//     Finish: function() {
-//         Pagination.e.innerHTML = Pagination.code;
-//         Pagination.code = '';
-//         Pagination.Bind();
-//     },
-
-//     // find pagination type
-//     Start: function() {
-//         if (Pagination.size < Pagination.step * 2 + 6) {
-//             Pagination.Add(1, Pagination.size + 1);
-//         } else if (Pagination.page < Pagination.step * 2 + 1) {
-//             Pagination.Add(1, Pagination.step * 2 + 4);
-//             Pagination.Last();
-//         } else if (Pagination.page > Pagination.size - Pagination.step * 2) {
-//             Pagination.First();
-//             Pagination.Add(Pagination.size - Pagination.step * 2 - 2, Pagination.size + 1);
-//         } else {
-//             Pagination.First();
-//             Pagination.Add(Pagination.page - Pagination.step, Pagination.page + Pagination.step + 1);
-//             Pagination.Last();
-//         }
-//         Pagination.Finish();
-//     },
-
-
-
-//     // --------------------
-//     // Initialization
-//     // --------------------
-
-//     // binding buttons
-//     Buttons: function(e) {
-//         var nav = e.getElementsByTagName('a');
-//         nav[0].addEventListener('click', Pagination.Prev, false);
-//         nav[1].addEventListener('click', Pagination.Next, false);
-//     },
-
-//     // create skeleton
-//     Create: function(e) {
-
-//         var html = [
-//             '<a>&#9668;</a>', // previous button
-//             '<span></span>', // pagination container
-//             '<a>&#9658;</a>' // next button
-//         ];
-
-//         e.innerHTML = html.join('');
-//         Pagination.e = e.getElementsByTagName('span')[0];
-//         Pagination.Buttons(e);
-//     },
-
-//     // init
-//     Init: function(e, data) {
-//         Pagination.Extend(data);
-//         Pagination.Create(e);
-//         Pagination.Start();
-//     }
-// };
-
-
+// This function fetches the JSON Data
 function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
@@ -162,6 +23,8 @@ function init() {
 }
 init();
 
+
+// This code renders the table
 function renderTable(data) {
 
     var table = document.createElement("TABLE");
@@ -169,10 +32,7 @@ function renderTable(data) {
     table.border = "1";
     var tableHeader = ["Payment ID", "Date", "Merchant ID", "Email ID", "Amount", "Status"];
     table.className += ' data-table';
-    //Get the count of columns.
     var columnCount = Object.keys(data[0]).length;
-    // table.setAttribute("data-table", "order-table");
-    //Add the header row.
     var row = table.insertRow(-1);
     for (var i = 0; i < columnCount; i++) {
         var headerCell = document.createElement("TH");
@@ -197,14 +57,6 @@ function renderTable(data) {
     dvTable.innerHTML = "";
     dvTable.appendChild(table);
 
-    Pagination.Init(document.getElementById('pagination'), {
-        size: data.length / 3, // pages size
-        page: 1, // selected page
-        step: 3 // pages before and after current
-    });
-    Pagination.Click(data, function() {
-        debugger;
-    })
 }
 
 // Sort Table
